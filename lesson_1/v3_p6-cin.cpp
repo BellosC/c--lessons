733c7f3d552cc1cffe364697b79fa877ef743080
Konstantinos Bellos <costas.bellos@hotmail.com>
2021-03-25 21:56:48 +0200

    p6

---
+++ lesson_1/v3_p6-cin.cpp
@@ -0,0 +1,17 @@
+/*CPP1.cin.cpp Χρηση της cin */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    double y = 5.01;
+    
+    cout<<"Dwse x: ";
+    cin>>x;                   // eisagwgi apo pliktrologio, prosoxi, telestis eisodou to ">>".
+    cout<<endl;
+    cout<<"To x einai: "<<x;
+    
+    return 0;
+}
