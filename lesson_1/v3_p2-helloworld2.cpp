3f25c574eff44f4ca0ca54b693136f56ae37472a
Konstantinos Bellos <costas.bellos@hotmail.com>
2021-03-25 21:10:03 +0200

    p2

---
+++ lesson_1/v3_p2-helloworld2.cpp
@@ -0,0 +1,12 @@
+/*CPP1.helloworld2.cpp using std::cout*/
+
+#include <iostream>
+using std::cout; /*me tin odigeia "using", ensomatonoume ena stoixeio (antikeimeno, stathera klp) wste na mh grafoume kathe fora
+                 pou vrisketai to antikeimeno cout */
+
+int main()
+{
+    cout<<"Hello World!\n";
+    cout<<"Hello2";
+    return 0;
+}
