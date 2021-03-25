62e7a2554a98d58d11371f9e54d09d506dd6f720
Konstantinos Bellos <costas.bellos@hotmail.com>
2021-03-25 23:38:02 +0200

    var2

---
+++ c_flashback/c_v11_p1-metavlites2.c
@@ -0,0 +1,24 @@
+/*variables.c: pou dilwnoume metavlites*/
+
+#include <stdio.h>
+
+int function(int x); //To -prwtotypo- mias sinartisis
+
+int g;               //H g einai -katholiki-. Se aftin exei prosvasi kai i main kai i sinartisi f
+
+main()
+{
+    int m;           //H m einai -topiki-. Se aftin exei prosvasi mono i main
+    /* 
+    ...entoles tis main...
+    */
+    
+}
+
+int function(int x)  //To -swma- tis sinartisis
+{
+    int k;           //H k einai -topiki- metavliti. Se aftin exei prosvasi mono i sinartisi f() */
+    /*
+    ...entoles tis function...
+    */
+}
