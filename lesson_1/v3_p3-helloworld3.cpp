1b3a6de3aa079e937a2a15b3e5005822c70393f1
Konstantinos Bellos <costas.bellos@hotmail.com>
2021-03-25 21:27:01 +0200

    p3

---
+++ lesson_1/v3_p3-helloworld3.cpp
@@ -0,0 +1,17 @@
+/*CPP1.helloworld3.cpp Ενσωμάτωση όλης της std*/
+
+/*me tin odigia "namespace std" stin ousia leme oti gia ola ta pragmata pou periexei i std, 
+gia kanena den xreiazetai na grafoume oti anikei ston sigkekrmeno xwro onomatwn (vlepe v3_p2-helloworld2.cpp)*/
+
+/*i xrisi tou "namespace" theoreitai tempeliki lisi, pou epivarinei to programma kai kalo einai na apofevgetai*/
+
+/* wstoso i xrisi tou "namespace" gia mikra programmata opws afto, den apotelei provlima */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World!";
+    return 0;
+}
