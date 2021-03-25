650a3688d75f39a75b05fc59917e04101fd796af
Konstantinos Bellos <costas.bellos@hotmail.com>
2021-03-25 19:15:59 +0200

    p1

---
+++ lesson_1/v3_p1-helloworld.cpp
@@ -0,0 +1,11 @@
+/* CPP1.helloworld.cpp*/
+
+#include <iostream> //proepexergastis
+
+int main() //simeio eisodou tou programmatos - tha iparxei panta mazi me ta agistra tis
+{
+    std::cout<<"Hello World!"; ///std = xwros onomatwn, cout = antikeimeno tou xwrou onomatwn
+    return 0;  //panta teleftaia grammi stin main
+}
+
+ 
