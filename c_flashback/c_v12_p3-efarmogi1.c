b29b01060c8a01961091345d11aca84b0e6e7fee
Konstantinos Bellos <costas.bellos@hotmail.com>
2021-03-26 11:04:32 +0200

    app1

---
+++ c_flashback/c_v12_p3-efarmogi1.c
@@ -0,0 +1,16 @@
+/* c -- metavlites - efarmogi 1*/
+#include <stdio.h>
+
+int main()
+{
+    const int my_number=100;
+    int akeraios;
+    
+    
+    printf("Dwse akeraio: ");
+    scanf("%d", &akeraios);
+    
+    printf("timi statheras: %d, timi akeraias metavlitis: %d", my_number, akeraios);
+
+    return 0;
+}
